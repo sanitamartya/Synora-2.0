ad0daa5c56794ab0dd74c47d9b917ba7b0646358
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-27 08:42:50 +0530

    Add JSON request parsing middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   response.send("Synora server is running");
+});
+
+app.post("/echo", (request, response) => {
+  response.json(request.body);
 });
 
 module.exports = app;
